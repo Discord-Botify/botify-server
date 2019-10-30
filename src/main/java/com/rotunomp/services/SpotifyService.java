451745faa0f35092b4906e15272f0f9dc933add8
Michael Rotuno <rotunomp@miamioh.edu>
2019-10-30 15:11:11 -0400

    First commit to new branch

--- src/main/java/com/rotunomp/services/SpotifyService.java
+++ src/main/java/com/rotunomp/services/SpotifyService.java
@@ -64,26 +64,6 @@ public class SpotifyService {
             System.out.println("Error connecting to Spotify");
         }
 
-        /* Add callbacks to handle success and failure */
-//        Futures.addCallback(responseFuture, new FutureCallback<ClientCredentials>() {
-//            @Override
-//            public void onSuccess(ClientCredentials clientCredentials) {
-//                /* The tokens were retrieved successfully! */
-//                System.out.println("Successfully retrieved an access token! " + clientCredentials.getAccessToken());
-//                System.out.println("The access token expires in " + clientCredentials.getExpiresIn() + " seconds");
-//
-//                /* Set access token on the Api object so that it's used going forward */
-//                api.setAccessToken(clientCredentials.getAccessToken());
-//
-//                /* Please note that this flow does not return a refresh token. * That's only for the Authorization code flow */
-//            }
-//
-//            @Override
-//            public void onFailure(Throwable throwable) {
-//                /* An error occurred while getting the access token. This is probably caused by the client id or * client secret is invalid. */
-//                System.out.println("Error connecting to Spotify");
-//            }
-//        });
         // Set up Hibernate environment
         sessionFactory = SessionFactoryInstance.getInstance();
     }
